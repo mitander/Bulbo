c14077142d2cec7a23008f5ad66ffee30d2b28f4
Ollejj <ollejohansson1996@gmail.com>
2020-05-13 22:46:47 +0200

    Hoppas det funkar

---
+++ src/main/java/sysjm3/bulbo/Bulbo/BulboApplication.java
@@ -0,0 +1,14 @@
+package sysjm3.bulbo.Bulbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulboApplication.class, args);
+		System.out.println("YAY");
+	}
+
+}
