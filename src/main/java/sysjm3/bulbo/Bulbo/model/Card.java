86a1eea2edd0b1a7e4c5a512667b390a12c81112
Christoffer Hansen <chris.hansen.ch@outlook.com>
2020-05-15 00:15:00 +0200

    Rest API Update 1.0
    Renamed package names to follow package convention
    
    Moved MainController to the controller package
    
    Deleted the CardsController
    
    Created package for exceptions
    Created CardNotFoundException file and its respective advice file called CardNotFoundAdvice
    Created WorkspaceNotFoundException file and its respective advice file called WorkspaceNotFoundAdvice
    Created a new JpaController
    Created a new model for the Workspace
    Created new repository for the Workspace model
    
    Modified the Card model extensively
    Modified the application.properties and the server.port value back to 8080
    
    Issue: Whenever you make a new Card you end up making thousands in one go. NEEDS HOTFIX IMMEDIETLY!

--- src/main/java/sysjm3/bulbo/Bulbo/model/Card.java
+++ src/main/java/sysjm3/bulbo/Bulbo/model/Card.java
@@ -4,43 +4,102 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="cards")
-public class Card implements Serializable{
+@Table(name = "cards")
+public class Card implements Serializable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	private int id;
-	
-	@Column(name="tag")
-	private String tag;
-	
-	public int getId() {
-		return id;
-	}
+    public Card() {
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public Card(String name, String content, Workspace workspace) {
+        this.name = name;
+        this.content = content;
+        this.workspace = workspace;
+    }
 
-	public String getTag() {
-		return tag;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "ID")
+    private long id;
 
-	public void setTag(String tag) {
-		this.tag = tag;
-	}
+    @Column(name = "name", nullable = false)
+    private String name;
 
-	public Card(String tag) {
-		super();
-		this.tag=tag;
-	}
-	
-	
-	
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "workspace_id", referencedColumnName = "ID", nullable = false)
+    private Workspace workspace;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Workspace getWorkspace() {
+        return workspace;
+    }
+
+    public void setWorkspace(Workspace workspace) {
+        this.workspace = workspace;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Card other = (Card) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" + "id=" + id + ", name=" + name + ", content=" + content + ", workspace=" + workspace + '}';
+    }
+
 }
