21004359a1d68d4fd358f08437704dd6cf2a4bca
Christoffer Hansen <chris.hansen.ch@outlook.com>
2020-05-15 16:03:08 +0200

    Removed unused imports

--- src/main/java/sysjm3/bulbo/Bulbo/model/Card.java
+++ src/main/java/sysjm3/bulbo/Bulbo/model/Card.java
@@ -2,7 +2,6 @@ package sysjm3.bulbo.Bulbo.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import java.io.Serializable;
-import javax.persistence.CascadeType;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
